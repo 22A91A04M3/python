39a93bcf067ab5b3443d3f41bcd65c7e4816b53c
22A91A04M3 <119720475+22A91A04M3@users.noreply.github.com>
2025-01-25 10:17:59 +0530

    Add files via upload

---
+++ array.py
@@ -0,0 +1,21 @@
+#printing Hello World
+#print(10)
+#print("pavan")
+#print(10, 20, 30, 40, sep=',')
+#print(10, 2.6, "Gayatri", [10, 20])
+#print(10, 20, 30, 40, 50, sep='\n')
+#print("Hello")
+#print(10, 20, end = ' ')
+#print(40, 50, "Gayatri")
+#print(70, 80, end = 'k')
+#print(12, 13, 14.2)
+a = 10
+b = 2
+print("Sum is:", a + b)
+print("Difference is:", a - b)
+print("Product is:", a * b)
+print("Quotient is:", a / b)
+print("Remainder is:", a % b)
+print(
+
+
