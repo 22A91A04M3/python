5e3ee97c91cb3fdc34a97ce1657390fbd1cc6634
22A91A04M3 <119720475+22A91A04M3@users.noreply.github.com>
2025-01-30 10:36:01 +0530

    Add files via upload

---
+++ valuebaselist.py
@@ -0,0 +1,17 @@
+####numbers = [1, 7, -12, 16, 18, 20, 30]
+####for i in numbers:
+####    print(i ** 2, end = ' ')
+##cities = ["Mumbai", "Hyderabad", "Chennai", "Kolkata"]
+##for i in range(len(cities)):
+##    print(cities[i], end = ' ')
+##print()
+##for city in cities:
+##    print(city, end = ' ')
+cities = ["Mumbai", "Hyderabad", "Chennai", "Kolkata"]
+for i in cities:
+    i = "Surampalem"
+print(cities)
+print()
+for i in range(len(cities)):
+    cities[i] = "Surampalem"
+print(cities)
