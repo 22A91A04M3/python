ca73c3baf24936c2edc59e1e18e617487fa631b2
22A91A04M3 <119720475+22A91A04M3@users.noreply.github.com>
2025-02-04 11:18:32 +0530

    Add files via upload

---
+++ test2.py
@@ -0,0 +1,17 @@
+"""
+a = 10
+b = 25
+c = a + b
+#print("sum of", a, "and", b, "is", c)
+#using %formatting
+#print("sum of %d and %d is %d"%(a, b, c))
+#using .format() method on strings
+#print("sum of {} and {} is {}".format(a, b, c))
+#using f strings (version 3.6)
+print(f"sum of {a} and {b} is {c}")
+"""
+x = 45.56455678
+print(x)
+print("%.4f"%x)
+print("{:.2f}".format(x))
+print(f"{x:.3f}")
